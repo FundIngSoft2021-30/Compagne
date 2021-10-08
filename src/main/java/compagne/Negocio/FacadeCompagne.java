3289531651990455e3a64d3155f1c340ff8cd93b
nclsbayona <nbayonaord@gmail.com>
2021-10-07 21:59:46 -0500

    facade

--- src/main/java/compagne/Negocio/FacadeCompagne.java
+++ src/main/java/compagne/Negocio/FacadeCompagne.java
@@ -1,28 +1,56 @@
 package compagne.Negocio;
 
+import java.util.HashSet;
+
+import compagne.Entidades.Comentario;
+import compagne.Entidades.Estudiante;
+import compagne.Entidades.Profesor;
+import compagne.IntegracionDatos.ControlProfesores;
+import compagne.IntegracionDatos.ControlEstudiantes;
+
 public class FacadeCompagne {
-    
-    public void crearPerfilProfesor(){
+    private ControlProfesores controlProfesores;
+    private ControlEstudiantes controlEstudiantes;
 
+    public FacadeCompagne() {
+        this.controlEstudiantes = new ControlEstudiantes();
+        this.controlProfesores = new ControlProfesores();
     }
 
-    public void modificarPerfilProfesor(){
-
+    public Profesor crearPerfilProfesor(String nombre, String experiencia, String email, String contrasenia,
+            HashSet<String> horariosAtencion, HashSet<String> materias, HashSet<Comentario> comentarios,
+            HashSet<String> logros) {
+        Profesor profesor = new Profesor(nombre, email, materias, comentarios, experiencia, contrasenia,
+                horariosAtencion, logros);
+        this.controlProfesores.crearProfesor(profesor);
+        return profesor;
     }
 
-    public void eliminarPerfilProfesor(){
-
+    public Profesor modificarPerfilProfesor(String nombre, String experiencia, String email, String contrasenia) {
+        // Email no se puede cambiar
+        Profesor profesor = new Profesor(nombre, email, null, null, experiencia, contrasenia, null, null);
+        this.controlProfesores.modificarProfesor(profesor);
+        return profesor;
     }
 
-    public void crearPerfilEstudiante(){
-
+    public boolean eliminarPerfilProfesor(String email) {
+        return this.controlProfesores.eliminarProfesor(email);
     }
 
-    public void modificarPerfilEstudiante(){
-
+    public Estudiante crearPerfilEstudiante(String nombre, String email, String contrasenia, HashSet<String> materias,
+            HashSet<Comentario> comentarios, HashSet<String> intereses, HashSet<String> logros) {
+        Estudiante estudiante = new Estudiante(nombre, email, materias, comentarios, contrasenia, intereses, logros);
+        this.controlEstudiantes.crearEstudiante(estudiante);
+        return estudiante;
     }
 
-    public void eliminarPerfilEstudiante(){
-        
+    public Estudiante modificarPerfilEstudiante(String nombre, String email, String contrasenia) {
+        Estudiante estudiante = new Estudiante(nombre, email, null, null, contrasenia, null, null);
+        this.controlEstudiantes.crearEstudiante(estudiante);
+        return estudiante;
+    }
+
+    public boolean eliminarPerfilEstudiante(String email) {
+        return this.controlEstudiantes.eliminarEstudiante(email);
     }
 }
