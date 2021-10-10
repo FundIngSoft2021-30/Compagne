bfa7b487748b4a44c1e48dee50da43b0cf9c4622
nclsbayona <nbayonaord@gmail.com>
2021-10-10 13:16:17 -0500

    ok

--- src/main/java/compagne/Negocio/PantallaGruposController.java
+++ src/main/java/compagne/Negocio/PantallaGruposController.java
@@ -16,7 +16,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class PantallaGruposController implements Initializable {
