f898c1f7d7b5b877c7e1345d9b304f3aecb459c4
nclsbayona <nbayonaord@gmail.com>
2021-10-11 18:56:18 -0500

    .

--- src/main/java/compagne/Negocio/VerInfoGrupoController.java
+++ src/main/java/compagne/Negocio/VerInfoGrupoController.java
@@ -1,16 +1,23 @@
 package compagne.Negocio;
 
+import javafx.scene.control.Alert;
 import compagne.Entidades.Grupo;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import compagne.Entidades.Usuario;
 
 public class VerInfoGrupoController {
 
+    private FacadeCompagne facade = new FacadeCompagne();
     private Grupo g;
+
+    @FXML
+    private ListView<Usuario> integrantes_list;
 
     @FXML
     private ResourceBundle resources;
@@ -35,7 +42,28 @@ public class VerInfoGrupoController {
 
     @FXML
     void addIntegrante(ActionEvent event) {
-        //TODO
+        if (this.email1.getText().length() > 0) {
+            if (this.facade.agregarUsuarioAGrupo(this.g.getCodigo(), this.email1.getText(), "N")) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Correcto");
+                alert.setHeaderText("Ingreso correctamente");
+                alert.setContentText("Enhorabuena!");
+                alert.showAndWait();
+                this.actualizar();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("No se pudo añadir");
+                alert.setContentText("Intentelo nuevamente...");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No se pudo añadir");
+            alert.setContentText("Intentelo nuevamente...");
+            alert.showAndWait();
+        }
     }
 
     @FXML
@@ -50,7 +78,8 @@ public class VerInfoGrupoController {
     }
 
     public void actualizar(){
-        //TODO
+        this.integrantes_list.getItems().clear();
+        this.integrantes_list.getItems().addAll(this.facade.listarCompasGrupo(this.g.getCodigo()));
     }
 
     public void start(Grupo g) {
