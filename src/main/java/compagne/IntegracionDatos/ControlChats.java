246ba6fe017f4f21faea8f6be39f523bb19aee8a
nclsbayona <nbayonaord@gmail.com>
2021-11-17 15:24:42 -0500

    .

--- src/main/java/compagne/IntegracionDatos/ControlChats.java
+++ src/main/java/compagne/IntegracionDatos/ControlChats.java
@@ -83,6 +83,7 @@ public class ControlChats {
         } catch (Exception e) {
         }
         query = "DELETE FROM "+ConnectionClass.getSchema() +"\"Chats\" WHERE \"ID\"=" + chatID + ";";
+        this.executeQuery(query);
         return result;
     }
 
